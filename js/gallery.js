05c5a286ed3d7b0c2222eca0b965b65e02169eca
TingboW <104877939+TingboW@users.noreply.github.com>
2022-06-04 23:15:18 -0700

    created a jQuery lightbox

--- js/gallery.js
+++ js/gallery.js
@@ -43,3 +43,28 @@ function on(i) {
 function off() {
     document.getElementById("overlay").style.display = "none";
 }
+
+$(document).ready(function(){
+    /* Open lightbox on button click */
+    $('li img').click(function(){
+        $('.backdrop').animate({'opacity':'.50'}, 300, 'linear').css('display', 'block');
+        $('.box').fadeIn();
+
+        //Check if lightbox has an image
+        if ($('.box').contents('img')) {
+            $('.box').contents().remove('img'); //If true, clear image
+        }
+
+            var img = $(this).clone(); //Duplicate DOM element
+            $('.box').append(img); //Insert duplicated element in another element
+    });
+
+    /* Click to close lightbox */
+    $('.close, .backdrop').click(function(){
+        $('.backdrop').animate({'opacity':'0'}, 300, 'linear', function(){
+            $('.backdrop').css('display', 'none');
+        });
+        $('.box').fadeOut();
+    });
+
+});
